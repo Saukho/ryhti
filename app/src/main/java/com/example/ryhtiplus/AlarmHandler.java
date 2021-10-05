2a093b67a5480d83428259965c2fd4cbd32ba977
RomanProkh <roman.prokhozhev@metropolia.fi>
2021-10-05 22:05:02 +0300

    Uudistunneet ilmoitukset

--- app/src/main/java/com/example/ryhtiplus/AlarmHandler.java
+++ app/src/main/java/com/example/ryhtiplus/AlarmHandler.java
@@ -14,56 +14,23 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class AlarmHandler {
-    private ArrayList<Alarm> alarms;
     private Intent intent;
     private PendingIntent pendingIntent;
     private Context context;
     private AlarmManager alarmManager;
+    private int notificationId = 1;
 
     public AlarmHandler(Context context, AlarmManager alarmManager){
-        alarms = new ArrayList<>();
-
         this.context = context;
         this.alarmManager = alarmManager;
     }
 
     public void setNewAlarm(Alarm a){
-        deleteAlarmsWithSameId(a);
         intent = new Intent(context, AlarmReciver.class);
-
-        //asetan muutaman arvon jotka voin vetää ulos onRecive() alarmReciveressä
-        //tää lähettää vissiin oikeaa tietoa mut alarmreciver ei vastaanota sitä emt miks
-        intent.putExtra("titleText", a.getNotifTitle());
-        intent.putExtra("textText", a.getNotifText());
-        intent.putExtra("id", a.getId());
-        Log.d("intentti", "setNewAlarm: "+a.getNotifText()+": "+a.getNotifTitle()+ ": "+a.getId());
-
+        intent.putExtra("id", notificationId);
         pendingIntent = PendingIntent.getBroadcast(context, a.getId(), intent, 0);
         long currentime = Calendar.getInstance().getTimeInMillis();
-        alarms.add(a);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, currentime + a.getTime(), a.getTime(), pendingIntent);
-
-    }
-
-    public boolean checkIfAlarmIdExist(Alarm a){
-        for(int i = 0; i < alarms.size() -1 ; i++){
-            if(alarms.get(i).getId() == a.getId()){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void deleteAlarmsWithSameId(Alarm a) {
-        for (int i = 0; i < alarms.size() - 1; i++) {
-            if (alarms.get(i).getId() == a.getId()) {
-                intent = new Intent(context, AlarmReciver.class);
-
-                pendingIntent = PendingIntent.getBroadcast(context, a.getId(), intent, 0);
-                alarmManager.cancel(pendingIntent);
-                alarms.remove(i);
-            }
-        }
     }
 
 }
--- app/src/main/java/com/example/ryhtiplus/MainActivity.java
+++ app/src/main/java/com/example/ryhtiplus/MainActivity.java
@@ -9,6 +9,7 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,68 +29,70 @@ public class MainActivity extends AppCompatActivity {
     AlarmManager alarmManager;
     EditText setHour;
     EditText setMinute;
-    EditText setId;
-    Button alarmInfo;
     Switch isAlarmEnabled;
     Button setAlarmButton;
     Button cancleAlarmBtn;
-    EditText notifTitle;
-    EditText notifText;
     AlarmHandler alarmHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createNotificationChannel();
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         setHour = findViewById(R.id.setHour);
         setMinute = findViewById(R.id.setMinute);
-        setId = findViewById(R.id.alarmIdSetter);
-        alarmInfo = (Button) findViewById(R.id.getAlarmInfo);
         isAlarmEnabled = (Switch) findViewById(R.id.isAlarmEnabled);
         setAlarmButton = (Button) findViewById(R.id.setAlarmButton);
         cancleAlarmBtn = (Button) findViewById(R.id.cancleAlarmBtn);
-        notifText = (EditText) findViewById(R.id.alarmContextText);
-        notifTitle = (EditText) findViewById(R.id.alarmContextTitle);
-
         alarmHandler = new AlarmHandler(this, alarmManager);
-
-        setId.setText("1");
-        setMinute.setText("0");
-        setHour.setText("0");
+        isAlarmEnabled.setChecked(false);
     }
 
-
-    public void getAlarmInfoOnDisplay(View view){
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isAlarmEnabled.setChecked(true);
     }
 
     public void setAlarm(View view){
+        int hour = 0;
+        int minute = 0;
+        //Tarkistaa, että tunti on kokonaisluku, muuten se on 0
+        try {
+             hour = Integer.parseInt(setHour.getText().toString());
+        }catch (NumberFormatException ex) {
 
-        Toast.makeText(MainActivity.this, "ALARM ON", Toast.LENGTH_SHORT).show();
+        }
+        //Tarkistaa, että minuutit on kokonaisluku, muuten ilmoittaa siitä käyttäjälle
+        try {
+            minute = Integer.parseInt(setMinute.getText().toString());
+        }catch (NumberFormatException ex) {
+            Toast.makeText(MainActivity.this, "Anna minuutit kokonaislukuna", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        long setTime = turnHourToMls(Integer.valueOf(setHour.getText().toString())) + turnMinuteToMls(Integer.valueOf(setMinute.getText().toString()));
-
+        long setTime = turnHourToMls(hour) + turnMinuteToMls(minute);
         if(setTime  >= 60000){
-            Alarm alarm = new Alarm(Integer.valueOf(setId.getText().toString()), setTime, notifTitle.getText().toString(), notifText.getText().toString(), true);
+            Alarm alarm = new Alarm(1, setTime, "Notification","Text",true);
             alarmHandler.setNewAlarm(alarm);
         } else {
             setTime = 1000;
-            Alarm alarm = new Alarm(Integer.valueOf(setId.getText().toString()), setTime, notifTitle.getText().toString(), notifText.getText().toString(), true);
+            Alarm alarm = new Alarm(1, setTime,"Notification","Text",true);
             alarmHandler.setNewAlarm(alarm);
-
         }
+        isAlarmEnabled.setChecked(true);
+        Toast.makeText(MainActivity.this, "Muistutus on asetettu", Toast.LENGTH_SHORT).show();
     }
 
-    public void cancleAlarmById(View view){
-        Intent intent = new Intent(this, AlarmReciver.class);
-
+    public void cancleAlarm(View view){
+        //tätä pitää vielä muokata
+        /*Intent intent = new Intent(this, AlarmReciver.class);
         pendingIntent = PendingIntent.getBroadcast(this, Integer.valueOf(setId.getText().toString()) + 10, intent, 0);
         alarmManager.cancel(pendingIntent);
-        Toast.makeText(MainActivity.this, "ALARM OFF", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "ALARM OFF", Toast.LENGTH_SHORT).show();*/
+        isAlarmEnabled.setChecked(false);
+        Toast.makeText(MainActivity.this, "Muistutus on peruuttu", Toast.LENGTH_SHORT).show();
     }
-
     public long turnHourToMls(int h){
         long mls = h * 1000 * 60 * 60;
         return mls;
@@ -97,18 +100,6 @@ public class MainActivity extends AppCompatActivity {
     public long turnMinuteToMls(int m){
         long mls = m * 1000 * 60;
         return mls;
-    }
-
-    public void createNotificationChannel(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            CharSequence name = "alarm";
-            String description  = "description";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("alarm",name, importance);
-            channel.setDescription(description);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
     }
 
 }
