80caff3d3a82419bb3dd1f536ad5cd61329867e2
RomanProkh <roman.prokhozhev@metropolia.fi>
2021-10-05 22:05:02 +0300

    Uudistunneet ilmoitukset

--- app/src/main/java/com/example/ryhtiplus/AlarmReciver.java
+++ app/src/main/java/com/example/ryhtiplus/AlarmReciver.java
@@ -1,62 +1,57 @@
 package com.example.ryhtiplus;
 
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioAttributes;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Bundle;
 import android.os.Vibrator;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 public class AlarmReciver extends BroadcastReceiver {
-
-    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private  static final String CHANNEL_ID = "SAMPLE_CHANNEL";
+    private static Ringtone ringtone;
     @Override
     // implement onReceive() method
-    public void onReceive(Context context, Intent intent) {
-
-        //tää ei oikein halua saada sitä tietoa mitä laitoin mut kuitenkin se saa jotain tietoa emt mistä
-        String contentTitle = intent.getStringExtra("titleText");
-        String contentText = intent.getStringExtra("textText");
+    public void onReceive(Context context, Intent   intent) {
         int id = intent.getIntExtra("id",0);
+        NotificationManager notifManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //for API 26+
+            CharSequence channel_name = "My Notification";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channel_name, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.enableLights(true);
+            channel.enableVibration(true);
+            notifManager.createNotificationChannel(channel);
 
-        Log.d("intentti", "onReceive: "+intent.getExtras());
-
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, id , intent, 0);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "alarm")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(contentTitle)
-                .setContentText(contentText)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-        notificationManager.notify(id, builder.build());
-
-        Vibrator vibrator = (Vibrator) context.getSystemService(context.VIBRATOR_SERVICE);
-        vibrator.vibrate(4000);
-
-        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
-        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        if (alarmUri == null) {
-            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         }
+        //call MainActivity when notification is pressed
+        Intent mainIntent = new Intent(context, com.example.ryhtiplus.MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(
+                context, 0,mainIntent,0
+        );
 
-        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
-
-        ringtone.play();
+        //prepare notification
+        NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setContentTitle("Muista ylläpitämään ryhtiä!")
+                .setContentText("Tämä on muistutus!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(contentIntent)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setAutoCancel(true);
+        //notify
+        notifManager.notify(id, notifBuilder.build());
     }
-
 }
