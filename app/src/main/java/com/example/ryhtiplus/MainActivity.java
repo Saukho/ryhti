50ac3d9607a751286001008eaa7d0269497eea9c
Mihail_branch <mihailka@metropolia.fi>
2021-10-06 14:51:39 +0300

    Vielä korjasin lisäämällä Buttonille onClickListener.

--- app/src/main/java/com/example/ryhtiplus/MainActivity.java
+++ app/src/main/java/com/example/ryhtiplus/MainActivity.java
@@ -3,23 +3,29 @@ package com.example.ryhtiplus;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView buttonSetNotif;
+    private Button buttonSetNotif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-    public void onSetNotifActivityClicked(View view){
-        Intent nextActivity = new Intent(MainActivity.this, SetNotifActivity.class);
-        startActivity(nextActivity);
+        buttonSetNotif = (Button)findViewById(R.id.buttonMainSetNotifActivity);
+        buttonSetNotif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nextActivity = new Intent(MainActivity.this, SetNotifActivity.class);
+                startActivity(nextActivity);
+            }
+        });
+
 
     }
+
 }
