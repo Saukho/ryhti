2752d2431cd7351127a6d7c06e930ef98415b808
saukho <sami.sihvonen2@metropolia.figit config --global user.email sami.sihvonen2@metropolia.figit config --global user.email sami.sihvonen2@metropolia.fi>
2021-10-08 10:33:39 +0300

    ui ready for merge

--- app/src/main/java/com/example/ryhtiplus/ImageCardActivity.java
+++ app/src/main/java/com/example/ryhtiplus/ImageCardActivity.java
@@ -1,10 +1,10 @@
 package com.example.ryhtiplus;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ImageCardActivity extends AppCompatActivity {
 
